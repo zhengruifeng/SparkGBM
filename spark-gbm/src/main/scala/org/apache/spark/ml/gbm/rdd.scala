7afab8274bc60564001e6b14b0c299aa641a1255
zhengruifeng <ruifengz@foxmail.com>
2018-09-03 11:14:54 +0800

    update rdd ops

--- spark-gbm/src/main/scala/org/apache/spark/ml/gbm/rdd.scala
+++ spark-gbm/src/main/scala/org/apache/spark/ml/gbm/rdd.scala
@@ -46,55 +46,6 @@ private[gbm] class PartitionSelectedRDD[T: ClassTag](@transient val parent: RDD[
 }
 
 
-private[gbm] class PartitionSampledRDDPartition[T](val idx: Int,
-                                                   val prev: Partition,
-                                                   val weight: Double,
-                                                   val seed: Long)
-  extends Partition with Serializable {
-
-  require(weight >= 0 && weight <= 1)
-
-  override val index: Int = idx
-}
-
-private[gbm] class PartitionSampledRDD[T: ClassTag](@transient val parent: RDD[T],
-                                                    val weights: Map[Int, Double],
-                                                    @transient private val seed: Long = System.nanoTime)
-  extends RDD[T](parent) {
-
-  require(weights.keys.forall(_ >= 0))
-  require(weights.values.forall(w => w >= 0 && w <= 1))
-
-  override def compute(split: Partition, context: TaskContext): Iterator[T] = {
-    val part = split.asInstanceOf[PartitionSampledRDDPartition[T]]
-
-    if (part.weight == 0) {
-      Iterator.empty
-    } else if (part.weight == 1) {
-      firstParent[T].iterator(part.prev, context)
-    } else {
-      val rng = new XORShiftRandom(part.seed)
-      firstParent[T].iterator(part.prev, context)
-        .filter(_ => rng.nextDouble < part.weight)
-    }
-  }
-
-  override def getPreferredLocations(split: Partition): Seq[String] =
-    firstParent[T].preferredLocations(split.asInstanceOf[PartitionSampledRDDPartition[T]].prev)
-
-  override protected def getPartitions: Array[Partition] = {
-    val rng = new XORShiftRandom(seed)
-    var idx = -1
-
-    firstParent[T].partitions
-      .filter(p => weights.contains(p.index))
-      .map { p =>
-        idx += 1
-        new PartitionSampledRDDPartition(idx, p, weights(p.index), rng.nextLong)
-      }
-  }
-}
-
 
 private[gbm] class RDDFunctions[T: ClassTag](self: RDD[T]) extends Serializable {
 
@@ -103,7 +54,20 @@ private[gbm] class RDDFunctions[T: ClassTag](self: RDD[T]) extends Serializable 
   }
 
   def samplePartitions(weights: Map[Int, Double], seed: Long): RDD[T] = {
-    new PartitionSampledRDD[T](self, weights, seed)
+    val (partIdArr, weightArr) = weights.toArray.unzip
+
+    selectPartitions(partIdArr)
+      .mapPartitionsWithIndex { case (pid, iter) =>
+        val w = weightArr(pid)
+        if (w == 0) {
+          Iterator.empty
+        } else if (w == 1) {
+          iter
+        } else {
+          val rng = new XORShiftRandom(pid + seed)
+          iter.filter(_ => rng.nextDouble < w)
+        }
+      }
   }
 
   def samplePartitions(fraction: Double, seed: Long): RDD[T] = {
